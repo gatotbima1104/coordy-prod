cc118836d5051bddbc152463b88bbb8f7a6a5edd
muhamad gatot <gatotbima2002@gmail.com>
2025-10-28 16:16:35 +0700

    fix: aasa

--- src/controllers/aasa.controller.ts
+++ src/controllers/aasa.controller.ts
@@ -1,23 +1,30 @@
 import { NextFunction, Request, Response } from "express";
 
-export class AasaController{
-    async signAasaApple(req: Request, res: Response, next: NextFunction) {
-        try {
-            console.log("hitted from aasa")
-            const aasa = {
-                applinks: {
-                    apps: ["7WUR3QM973.com.mario-panApp.Coordiy"],
-                },
-                appclips: {
-                    apps: ["7WUR3QM973.com.mario-panApp.Coordiy.Coordiy"]
-                }
+export class AasaController {
+  async signAasaApple(req: Request, res: Response, next: NextFunction) {
+    try {
+      console.log("hitted from aasa");
+
+      const aasa = {
+        applinks: {
+          apps: [],
+          details: [
+            {
+              appID: "7WUR3QM973.com.mario-panApp.Coordiy",
+              paths: [ "/appclip/*", "/event/*", "/vote/*", "/" ]
             }
-
-            res.setHeader("Content-Type", "application/json");
-            res.status(200).json(aasa);
-
-        } catch (error) {
-            next(error)
+          ]
+        },
+        appclips: {
+          apps: [ "7WUR3QM973.com.mario-panApp.Coordiy.Coordiy" ]
         }
-    }   
+      };
+
+      res.setHeader("Content-Type", "application/json");
+      res.setHeader("Cache-Control", "no-cache");
+      res.status(200).json(aasa);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
