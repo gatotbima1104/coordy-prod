28f75e3e50aca73018400d1aa54b6043f517f916
muhamad gatot <gatotbima2002@gmail.com>
2025-11-10 22:19:27 +0700

    fix: remove-isExpired

--- src/controllers/vote.controller.ts
+++ src/controllers/vote.controller.ts
@@ -28,17 +28,10 @@ export class VoteController {
         include: { participants: true },
       });
 
-      if (eventExist.isExpired) {
-        throw new Error("This event has expired. Voting is no longer allowed.");
-      }
-
-      if (eventExist.expiredAt && new Date() > new Date(eventExist.expiredAt)) {
+      if (new Date() > new Date(eventExist.expiredAt)) {
         await prisma.event.update({
           where: { 
             id: eventExist.id
-          },
-          data: { 
-            isExpired: true
           },
         });
         throw new Error("This event has expired. Voting is no longer allowed.");
