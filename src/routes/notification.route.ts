90909a7a474630319f2330c9871d6d85e1e824f5
muhamad gatot <gatotbima2002@gmail.com>
2025-11-15 14:36:26 +0700

    fix: email-to-calendar2

--- src/routes/notification.route.ts
+++ src/routes/notification.route.ts
@@ -139,7 +139,7 @@ export const notificationRouter = () => {
    *         description: Internal server error
    */
   router.patch("/:id", verifyToken, notificationRouter.updateNotification);
-  router.get("/calendar/:id.ics", notificationRouter.addToCalendar)
+  router.get("/calendar/:id", notificationRouter.addToCalendar);
 
   /**
    * @swagger
